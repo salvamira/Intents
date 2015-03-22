7fe67f0033c2e01f4902b5452bb88c1c9e5160f4
Salva Mira <smira@iesperemaria.com>
2015-03-22 19:23:26 +0100

    Commit inicial

---
+++ app/src/main/java/net/somtic/intents/MainActivity.java
@@ -0,0 +1,76 @@
+package net.somtic.intents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void pgWeb(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://campus.somtic.net/"));
+        startActivity(intent);
+    }
+
+    public void llamadaTelefono(View view) {
+        Intent intent = new Intent(Intent.ACTION_CALL,
+                Uri.parse("tel:966870700"));
+        startActivity(intent);
+    }
+
+
+    public void googleMaps(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("geo:38.553468,-0.121579"));
+        startActivity(intent);
+    }
+
+    public void hacerFoto(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+    }
+
+    public void mandarCorreo(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "asunto");
+        intent.putExtra(Intent.EXTRA_TEXT, "texto del correo");
+        intent.putExtra(Intent.EXTRA_EMAIL,
+                new String[] {"smira@iesperemaria.com" });
+        startActivity(intent);
+    }
+}
